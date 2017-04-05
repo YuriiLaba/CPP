488305a76514b0a9385625e5bf41d35fe1171041
Oleksandr Pryhoda <pryhoda@ucu.edu.ua>
2017-04-05 21:06:46 +0300

    Add files via upload

--- Python/main_version_raw_threads.py
+++ Python/main_version_raw_threads.py
@@ -1,7 +1,7 @@
+import multiprocessing
 from time import time, process_time
 from string import punctuation
-from collections import Counter
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 
 def read_file(file_name):
     words_list = []
@@ -20,49 +20,61 @@ def write_file(word_counter, file_name):
 
 
 class WordsCount(Process):
-    word_counter = Counter({})
+    lock = multiprocessing.Lock()
 
-    def __init__(self, words_list, word_counter):
+    def __init__(self, words_list):
         super().__init__()
         self.words_list = words_list
 
+
     def run(self):
-        local_dict = Counter({})
+
+        local_dict = {}
         for word in self.words_list:
             if word not in local_dict:
                 local_dict[word] = 1
             else:
                 local_dict[word] =+ 1
 
-        WordsCount.word_counter += local_dict
-        print(WordsCount.word_counter)
+        WordsCount.lock.acquire()
+        try:
+            for i in local_dict.keys():
+                if i in word_counter.keys():
+                    word_counter[i] += local_dict[i]
+
+                else:
+                    word_counter[i] = local_dict[i]
+        finally:
+            WordsCount.lock.release()
 
 
-def launch_program():
-    word_counter = Counter({})
-    threads = []
+
+with Manager() as manager:
+    word_counter = manager.dict()
+    processes = []
     input_list = read_file('text.txt')
     avg = len(input_list) / 10
     last = 0
 
+
     while last < len(input_list):
-        threads.append(WordsCount(input_list[int(last):int(last + avg)], word_counter))
+        processes.append(WordsCount(input_list[int(last):int(last + avg)]))
         last += avg
 
     start_time = time()
 
-    for thread in threads:
-        thread.start()
+    for process in processes:
+        process.start()
 
-    for thread in threads:
-        thread.join()
+    for process in processes:
+        process.join()
 
     print(process_time())
-
-    print('Got {} threads in {} seconds'.format(len(threads), time() - start_time))
+    print(word_counter)
+    print('Got {} threads in {} seconds'.format(len(processes), time() - start_time))
     write_file(word_counter, 'result.txt')
 
 
-launch_program()
+
 
 
